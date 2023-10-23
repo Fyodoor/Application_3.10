4b8dadc90e876a07f1dccb440707c389e021facc
Knyaz <rewppp6@gmail.com>
2023-10-23 21:20:36 +0300

    23.10.23 дз

---
+++ src/Lesson_06/Cat.java
@@ -0,0 +1,51 @@
+package Lesson_06;
+
+public class Cat {
+    String name;
+    int age;
+    String breed;
+    String color;
+
+
+    Cat() {
+
+    }
+    Cat(String name, int age, String breed, String color) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/Lesson_06/Main.java
@@ -0,0 +1,16 @@
+package Lesson_06;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat snezhok = new Cat();
+        snezhok.setName("Snezhok");
+        snezhok.setAge(5);
+        snezhok.setBreed("Siam cat");
+        snezhok.setColor("Black");
+
+        System.out.println("I have cat. His name is " + snezhok.getName());
+        System.out.println("Hes age is " + snezhok.getAge());
+        System.out.println("His breed is " + snezhok.getBreed());
+        System.out.println("Hi is " + snezhok.color);
+    }
+}
